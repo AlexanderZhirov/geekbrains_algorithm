222ea01e4bba748c195e3e987e391a2d85e3b300
Alexander Zhirov <azhirov1991@gmail.com>
2021-03-20 11:13:38 +0300

    lesson 5

---
+++ lesson_5/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+void exercise1();
+void exercise2();
+
+int main(const int argc, const char **argv)
+{
+	exercise1();
+	exercise2();
+
+	return 0;
+}
+
+void exercise1()
+{
+
+}
+
+void exercise2()
+{
+
+}
